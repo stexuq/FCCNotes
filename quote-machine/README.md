02d9d227d79512a38bfff09e1d3bfc48a83d3ba7
Qian <ee.xuqian@gmail.com>
2017-02-25 17:29:43 -0800

    finished

---
+++ quote-machine/README.md
@@ -0,0 +1,30 @@
+Run
+```
+python -m SimpleHTTPServer 8000
+```
+
+## 实现一个弹幕墙
+[FreeCodeCamp课程链接](http://freecodecamp.cn/challenges/build-a-random-quote-machine)
+[目标](http://codepen.io/freeCodeCamp/full/bELoPJ)
+**功能**：随机生成一个能产生格言警句的网页，点击刷新按钮后能够获得新一条格言；并且有一个按钮能把格言发送到twitter。
+
+**步骤**：按照结构->样式->功能 进行开发，重点在功能。
+
+### 结构
+一个简单结构，分为格言内容，更新按钮和推特按钮。
+
+### 样式
+简单的样式，在每次刷新格言时，会变换背景颜色、格言字体颜色和按钮颜色。
+
+### 功能
+####思考弹幕墙实现的过程：
+
+1. 更新格言按钮能更新一条格言,同时更新展示的样式。
+2. 分享到twitter的时候，用一个anchor元素指向了一个url，在点击该元素时动态生成分享的内容到url字串尾部。
+
+
+### 踩坑记录
+1. jQuery新手，$(document).ready()内部定义的函数，普通的onclick监听是不能读取到的。scope问题。
+
+### 心得
+1. Google dev console break point 好顶赞。
---
+++ quote-machine/quote.js
@@ -0,0 +1,60 @@
+var quots = [
+    "When you betray somebody else, you also betray yourself. -Isaac Bashevis Singer",
+    "So the lover must struggle for words. -T. S. Eliot",
+    "All good things which exist are the fruits of originality. -John Stuart Mill",
+    "Nature never makes any blunders, when she makes a fool she means it. -Archibald Alexander",
+    "Show me a good loser, and I'll show you a loser. -Vince Lombardi",
+    "I don't go around regretting things that don't happen. -Virgil Thomson",
+    "Love in its essence is spiritual fire. -Lucius Annaeus Seneca",
+    "Fine art is knowledge made visible. -Gustave Courbet",
+    "The most important thing about Spaceship Earth - an instruction book didn't come with it. -R. Buckminster Fuller",
+    "Genius is more often found in a cracked pot than in a whole one. -E. B. White",
+    "A pure hand needs no glove to cover it. -Nathaniel Hawthorne",
+    "The heart forgets its sorrow and ache. -James Russell Lowell",
+    "What is art but a way of seeing? -Saul Bellow",
+    "The fairest thing in nature, a flower, still has its roots in earth and manure. -D. H. Lawrence",
+    "Why don't you get a haircut? You look like a chrysanthemum. -P. G. Wodehouse"
+];
+
+var newquote = function() {
+  var idx = Math.floor(Math.random() * quots.length);
+  //console.log("idx = " + idx);
+  return quots[idx];
+}
+
+var getNewQuote = function() {
+  var newQuote = newquote();
+  // 改变quote-text里的文字
+  $("#text").html(newQuote);
+  var newColor = getRandomColor();
+  // 改变背景颜色，改变quo-text颜色，改变new-quote-btn颜色
+  document.body.style.backgroundColor = newColor;
+  document.getElementById("text").style.color = newColor;
+  document.getElementById("new-quote-btn").style.backgroundColor = newColor;
+} 
+
+//随机颜色函数
+var getRandomColor = function() {//随机颜色函数
+  return '#' + (function(h) {
+    return new Array(7 - h.length).join("0") + h
+  })((Math.random() * 0x1000000 << 0).toString(16))
+};
+
+// jQuery, 更新share content的内容
+$(document).ready(function() {
+  $("#tweet-quote").click(function(e){
+     e.preventDefault();//this will prevent the link trying to navigate to another page
+    var href = $(this).attr("href");//get the href so we can navigate later
+
+    //do the update
+    //console.log(href);
+
+    var quoteText = $("#text").text();
+    //console.log(quoteText);
+
+    href = "https://twitter.com/intent/tweet?text=" + encodeURI(quoteText);
+
+    //when update has finished, navigate to the other page
+    window.location = href;
+  });
+});
